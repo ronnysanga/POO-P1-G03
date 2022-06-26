28538eb1cc7ee796c3f263f93c8479e8e0c9f5e3
Ronny <rsanga@espol.edu.ec>
2022-06-26 12:27:35 -0500

    Cambio R

---
+++ TerapiasG3/src/main/java/ec/edu/espol/terapiasg3/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.terapiasg3;
+
+/**
+ *
+ * @author dell
+ */
+public class Cliente extends Persona{
+    private String datoRepres;
+
+    public Cliente(String datoRepres, String cedula, String nombre, String telefono, String email) {
+        super(cedula, nombre, telefono, email);
+        this.datoRepres = datoRepres;
+    }
+    
+    //Commentario 1
+    
+
+    public String getDatoRepres() {
+        return datoRepres;
+    }
+
+    public void setDatoRepres(String datoRepres) {
+        this.datoRepres = datoRepres;
+    }
+    
+    
+}
---
+++ TerapiasG3/src/main/java/ec/edu/espol/terapiasg3/Persona.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.terapiasg3;
+
+/**
+ *
+ * @author dell
+ */
+public class Persona {
+    private String cedula;
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Persona(String cedula, String nombre, String telefono, String email) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+      
+    
+}
